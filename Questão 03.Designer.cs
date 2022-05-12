600a79c3e547a5f48312e7cfd972567ccb820e2d
Gabriel <gabrielalvesps13@gmail.com>
2022-05-12 14:07:17 -0300

    bghh

---
+++ Questão 03.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace AppProva13GabrielAlves
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl = new System.Windows.Forms.Panel();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.pnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl
+            // 
+            this.pnl.Controls.Add(this.lbl4);
+            this.pnl.Controls.Add(this.txt4);
+            this.pnl.Controls.Add(this.lbl2);
+            this.pnl.Controls.Add(this.txt2);
+            this.pnl.Controls.Add(this.lbl3);
+            this.pnl.Controls.Add(this.txt3);
+            this.pnl.Controls.Add(this.btn_1);
+            this.pnl.Controls.Add(this.lbl1);
+            this.pnl.Controls.Add(this.txt1);
+            this.pnl.Location = new System.Drawing.Point(12, 23);
+            this.pnl.Name = "pnl";
+            this.pnl.Size = new System.Drawing.Size(371, 325);
+            this.pnl.TabIndex = 0;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(77, 27);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(38, 13);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "Nome:";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(138, 20);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 2;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Location = new System.Drawing.Point(93, 230);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(181, 34);
+            this.btn_1.TabIndex = 3;
+            this.btn_1.Text = "button1";
+            this.btn_1.UseVisualStyleBackColor = true;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(77, 111);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(38, 13);
+            this.lbl3.TabIndex = 4;
+            this.lbl3.Text = "Nome:";
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(138, 104);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 5;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(77, 70);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(38, 13);
+            this.lbl2.TabIndex = 6;
+            this.lbl2.Text = "Nome:";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(138, 63);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 7;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(77, 155);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(38, 13);
+            this.lbl4.TabIndex = 8;
+            this.lbl4.Text = "Nome:";
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(138, 148);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 20);
+            this.txt4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 593);
+            this.Controls.Add(this.pnl);
+            this.Name = "Form1";
+            this.Text = "Frm1";
+            this.pnl.ResumeLayout(false);
+            this.pnl.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txt1;
+    }
+}
+
